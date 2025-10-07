ab51de67d7934c8f7333835d1ea246cf0712afe1
Jontiq <99jonathancar@gmail.com>
2025-10-07 15:45:22 +0200

    Created the Main window, adding possibility to press the "Grid" and "StackPanel" buttons. I have also added their functions according to the assignment.

---
+++ Hemuppgift_WPFtemplate/GridWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hemuppgift_WPFtemplate
+{
+    /// <summary>
+    /// Interaction logic for GridWindow.xaml
+    /// </summary>
+    public partial class GridWindow : Window
+    {
+        public GridWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RowColumnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(ColumnInput.Text, out int column)&&(int.TryParse(RowInput.Text, out int row)))
+            {
+                if (column >= 0 && column <= 3 && row >= 0 && row <= 3)
+                {
+                    Grid.SetColumn(GreenBox, column);
+                    Grid.SetRow(GreenBox, row);
+                    BoxText.Text = $"Row {row}, Column {column}";
+                }
+                else
+                {
+                    MessageBox.Show("Please only enter numbers between 0-3.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter numerical values");
+            }
+        }
+    }
+}
--- Hemuppgift_WPFtemplate/MainWindow.xaml.cs
+++ Hemuppgift_WPFtemplate/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Hemuppgift_WPFtemplate
 {
@@ -7,10 +9,22 @@ namespace Hemuppgift_WPFtemplate
     /// </summary>
     public partial class MainWindow : Window
     {
-        //HEJ
+        //HEJ - HEJ?
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GridButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridWindow newGridWindow = new GridWindow();
+            newGridWindow.Show();
+        }
+
+        private void StackPanelButton_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanelWindow newStackPanelWindow = new StackPanelWindow();
+            newStackPanelWindow.Show();
         }
     }
 }
